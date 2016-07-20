2814102bdb84bd11a2528a00ae7cb456b758cc84
Kra1o5 <kra1o5x@gmail.com>
2016-07-20 21:12:03 +0200

    Updates from 4.1.0 release

--- cm.mk
+++ cm.mk
@@ -16,7 +16,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    TARGET_DEVICE=Aquaris_M5 PRODUCT_NAME=Aquaris_M5
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=bq/Aquaris_M5/Aquaris_M5:6.0.1/MMB29M/1463479394:user/release-keys \
-    PRIVATE_BUILD_DESC="piccolo-user 6.0.1 MMB29M 4.0.0.10_20160517-1203 release-keys"
+    BUILD_FINGERPRINT=bq/Aquaris_M5/Aquaris_M5:6.0.1/MMB29M/1468312140:user/release-keys \
+    PRIVATE_BUILD_DESC="piccolo-user 6.0.1 MMB29M 4.0.0.11_20160712-1029 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-bq
+
