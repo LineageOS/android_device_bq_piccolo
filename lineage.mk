07a076cbd5183f775393b7f0d386eb2b2276886b
Aymen <aymenbousselham@gmail.com>
2017-09-24 19:08:29 +0100

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/bq/piccolo/full_piccolo.mk)
 
 PRODUCT_RELEASE_NAME := Aquaris M5
-PRODUCT_NAME := lineage_piccolo
+PRODUCT_NAME := pa_piccolo
 
 # Set product device & name
 PRODUCT_BUILD_PROP_OVERRIDES += \
