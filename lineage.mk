f34faaa0ed87dc6a4133255430320250e4a69d8a
Aymende7 <aymenbousselham@gmail.com>
2018-01-12 13:27:20 +0000

    piccolo: vendor/cm -> vendor/lineage
    Change-Id: I9e5e63096027d2efc0da8812a39edf61e32beed4

--- lineage.mk
+++ lineage.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/bq/piccolo/full_piccolo.mk)
