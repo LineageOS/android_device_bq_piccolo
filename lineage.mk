80a5fd660cf193c303d0fd2fe66b0334dca07141
Eloi Carracedo <eloimuns@gmail.com>
2018-02-11 15:09:10 +0100

    piccolo: Update build fingerprint
    Change-Id: Ic3a2361b96112bccfca38460844696235a222f22

--- lineage.mk
+++ lineage.mk
@@ -16,7 +16,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    TARGET_DEVICE=Aquaris_M5 PRODUCT_NAME=Aquaris_M5
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=bq/Aquaris_M5/Aquaris_M5:6.0.1/MMB29M/1479371716:user/release-keys \
-    PRIVATE_BUILD_DESC="piccolo-user 6.0.1 MMB29M 4.0.0.11_20161117-0935 release-keys"
+    BUILD_FINGERPRINT=bq/Aquaris_M5/Aquaris_M5:7.1.2/N2G47H/1514477459:user/release-keys \
+    PRIVATE_BUILD_DESC="piccolo-user 7.1.2 N2G47H 5.0.0_20171228-1707 release-keyss"
 
 PRODUCT_GMS_CLIENTID_BASE := android-bq
