45f845b482b302d515e6718ac0ddcee474d7227b
aymende7 <aymenbousselham@gmail.com>
2018-04-26 20:10:35 +0200

    Revert "piccolo: Build media extensions"
    This is set globaly now
    
    This reverts commit 916492008193985316f5779e1e3cb38ef9548266
    
    Change-Id: I78dcd13f0db303134e83dec9df9f3f320d23da24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,9 +39,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-004a/en_gestu
 # Filesystem
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13013734522 # (BOARD_USERDATAIMAGE_PARTITION_SIZE - 16384 for crypto footer)
 
-# Media Extensions
-TARGET_USES_MEDIA_EXTENSIONS := true
-
 # Power
 TARGET_POWERHAL_SET_INTERACTIVE_EXT := $(DEVICE_PATH)/power/power_ext.c
 
