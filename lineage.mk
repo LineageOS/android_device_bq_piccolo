084517862b67ad3a912a11c6efce9242d99132eb
Nolen Johnson <johnsonnolen@gmail.com>
2018-06-09 21:05:00 +0000

    piccolo: Define Vendor security patch level
    * This is needed for Trust, as we don't have a Vendor image that sets a
      valid vendor security patch level.
    
    * Taken from From BQ 5.2.0 release.
    
    Change-Id: I33c9b0b15524bd65e82e19d5f26884da89ec29d4

--- lineage.mk
+++ lineage.mk
@@ -22,3 +22,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := bq/Aquaris_M5/Aquaris_M5:7.1.2/N2G47H/1517833600:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-bq
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2018-01-05
