4548c5b08a0c256174d215694058d30923cea948
Eloi Carracedo <eloimuns@gmail.com>
2019-03-25 18:47:09 +0100

    piccolo: Remove hwui properties
    Change-Id: Iad7b33dfe1e3c82057ec24b191d8641d86814d3f

--- device.mk
+++ device.mk
@@ -21,7 +21,6 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
